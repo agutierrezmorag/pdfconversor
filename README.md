51d8b25c71aca504a908de23508e088af27d9e73
agutierrezmorag <alvarogutierrezmoraga@gmail.com>
2023-10-02 15:25:52 -0300

    added testing and results

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # pdfconversor
 
 Conversor de PDF > imagen > texto.
+
+# Dependencias
+
+- [Poppler](https://github.com/oschwartz10612/poppler-windows/releases/)
+
+# Testing
+
+Se realizó la transcripción de las páginas 5-10 del reglamento regimen general de estudios, mediante una 
+[herramienta online](https://www.online-convert.com/) y luego de manera local con las librerías `pdf2image` para la separación 
+de cada página del pdf por imágenes y `easyocr` para la transcripción de dichas imágenes.
+
+Esto resulta en un texto con errores, para intentar corregir algunos se propone utilizar `gpt3.5` dando uso del
+siguiente prompt: 
+
+`Revisa el siguiente texto y corrígelo. Solo realiza correcciones ortográficas, elimina las oraciones sin sentido, 
+pero no cambies el orden ni la estructura del texto.`
+
+Sin embargo, algunos errores persisten, sobre todo con ciertos caracteres especiales.
+
+El tiempo de operación entre ambas opciones fue similar.
--- main.py
+++ main.py
@@ -7,20 +7,32 @@ import easyocr
 
 images = []
 reader = easyocr.Reader(['es'])
+results = []
 
+# se itera por la carpeta documentos
 for file in os.listdir('./documentos'):
+
+    # una vez seleccionado un documento, se crea una carpeta dentro de scans con su nombre
     filename, _ = file.split('.')
     if not os.path.exists(f'scans/{filename}'):
         os.makedirs(f'scans/{filename}')
-    images = convert_from_path(f'documentos/{file}', output_folder=f'scans/{filename}', fmt='jpeg')
 
-results = reader.readtext(images[0], detail=0, paragraph=True)
+    # se realiza la conversion a imagen y se almacenan dentro de la carpeta anteriormente creada
+    images = convert_from_path(
+        f'documentos/{file}',
+        output_folder=f'scans/{filename}',
+        fmt='jpeg',
+        poppler_path=r'C:\dev\poppler-23.08.0\Library\bin'
+    )
 
-text_lines = [text for (bbox, text, prob) in results]
+for i in range(5, 11):
+    results.append(reader.readtext(images[i], detail=0, paragraph=True))
 
-output_file = 'output.txt'  # Replace with your desired output file path
 
+output_file = 'easyocr.txt'
+
+# se escribe los resultados a un archivo .txt
 with open(output_file, 'w', encoding='utf-8') as file:
-    file.writelines('\n'.join(text_lines))
-
-
+    for result in results:
+        text = '\n'.join(result)
+        file.write(text + '\n\n')
